7bb1d633d005af099c0075a1b32db3c1ac378a10
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2024-09-25 09:09:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 let gridHolder = document.querySelector(".grid-holder");
 let container = document.querySelector('.container');
+const button = document.getElementById('submit-btn');
 
-function generateDivs(num) {
-    gridWidth = num; 
+function generateDivs() {
+  
+    gridWidth = gridSize(); 
     color = random_rgb();
     let i = 0;
+    //create the grid spans
     while (i < gridWidth ** 2){
         let box = document.createElement('div');
+        box.innerText = ""
         box.classList.add('grid')
-        box.style.background = color
+        box.style.background = "grey"
+        box.style.width = `${40 / gridWidth}rem`;
+        box.style.height = `${40 / gridWidth}rem`;
         container.appendChild(box);
+        box.addEventListener("mouseover", function (){
+        box.style.background = color;
+        })
+        box.addEventListener("touchmove", function() {
+          box.style.background = color;
+        });
         i++
     }
-    color = random_rgb();
-}
-
+     
+  }
 
 function random_rgb() {
   let red = Math.floor(Math.random() * 256);
@@ -22,4 +33,21 @@ function random_rgb() {
   let blue = Math.floor(Math.random() * 256);
   return `rgb(${red}, ${green}, ${blue})`
 }
-generateDivs(16);
+
+function gridSize() {
+  const value = document.getElementById("myRange").value;
+  return value;
+}
+generateDivs()
+button.addEventListener("click", function() {
+  removeElementsByClass("grid");
+  generateDivs();
+})
+
+//remove elements by class
+function removeElementsByClass(className) {
+  let elements = document.getElementsByClassName(className);
+  while(elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
+  }
+}
